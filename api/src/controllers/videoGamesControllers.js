38a9bed2f2d84c0b514fb8150b4b8621c9cbd335
Maria Salazar <mvsalazarflores@gmail.com>
2023-06-01 01:38:46 -0300

    first route done

--- api/src/controllers/videoGamesControllers.js
+++ api/src/controllers/videoGamesControllers.js
@@ -1,5 +1,92 @@
-const { Videogame } = require('../db');
-const {API_KEY} = process.env
+const { Videogame, Genre } = require('../db');
+const axios = require('axios')
+const {Op} = require('sequelize')
+require('dotenv').config()
+const { API_KEY } = process.env;
+
+function transformGames(games) {
+  return games.map((game) => {
+    const genres = game.genres.map((g) => g.name);
+    const platforms = game.platforms.map((p) => p.platform);
+    return {
+      id: game.id,
+      image: game.background_image,
+      name: game.name,
+      description: game.description,
+      genres: genres,
+      platforms: platforms,
+      rating: game.rating,
+      released: game.released
+    };
+  });
+}
+
+const cleanApiArray = async (name) => {
+  name = name.split(' ').join('-').toLowerCase();
+  //traigo los juegos de la api ordenandolos en 50 por página
+  const response = await axios.get(`https://api.rawg.io/api/games?search=${name}&key=${ API_KEY }&page_size=100`);
+  //ordeno la info de la api para que tenga las mismas características que la DB
+  const apigames = response.data.results;
+  const transformedGames = transformGames(apigames);
+  return transformedGames;
+};
+
+const cleanApiArray2 = async () => {
+  
+  //traigo los juegos de la api ordenandolos en 100 por página
+  const response = await axios.get(`https://api.rawg.io/api/games?key=${API_KEY}&page_size=100`);
+
+
+  //ordeno la info de la api para que tenga las mismas características que la DB
+  const apigames = response.data.results;
+  const transformedGames = transformGames(apigames);
+  return transformedGames;
+};
+
+const dataBasebyName = async (name) => {
+  return await Videogame.findAll({
+    include: [Genre],
+    //incluyendo también la información del modelo relacionado Genre 
+    where: {
+      //lo busca por el nombre pudiendo terminar en algo inexacto
+      name: { [Op.like]: `${name}%` }
+    }
+  });
+};
+
+const dataBase = async () => {
+  return await Videogame.findAll({
+    include: [Genre],
+    //incluyendo también la información del modelo relacionado Genre 
+  });
+};
+
+        
+
+
+
+
+const getGamesByName = async (name) => {
+  //en esta función junto toda la información de la API más la base de datos
+  const apiData = await cleanApiArray(name);
+  const dbData = await dataBasebyName(name);
+  const totalResult = [...apiData, ...dbData];
+
+ const first15Elements = totalResult.slice(0, 16);
+
+  return first15Elements;
+
+  
+}
+
+const getAllGames = async () => {
+  const apiData = await cleanApiArray2();
+  const dbData = await dataBase();
+  const totalResult = [...apiData, ...dbData];
+
+  return totalResult;
+}
+
 
 
 const createVideogame = async () => {
@@ -17,5 +104,8 @@ const createVideogame = async () => {
 
 
 module.exports = {
-  createVideogame
+  createVideogame,
+  cleanApiArray,
+  getGamesByName,
+  getAllGames
 }
--- api/src/handlers/videogamesHandlers.js
+++ api/src/handlers/videogamesHandlers.js
@@ -1,6 +1,8 @@
-const axios = require('axios');
+//const axios = require('axios');
+const { getGamesByName, getAllGames } = require('../controllers/videoGamesControllers');
 require('dotenv').config()
 const { API_KEY } = process.env;
+
 
 const getVideogamesHandler = async (req, res) => {
   //***Obtiene un arreglo de objetos, donde cada objeto es un videojuego con su información
@@ -10,19 +12,27 @@ const getVideogamesHandler = async (req, res) => {
 // Debe poder buscarlo independientemente de mayúsculas o minúsculas.
 // Si no existe el videojuego, debe mostrar un mensaje adecuado.
 // Debe buscar tanto los de la API como los de la base de datos.
+  const { name } = req.query;
+  try {
+    const gameResult = name ?
+      await getGamesByName(name) :
+      await getAllGames();
+    res.status(200).json(gameResult)
+  } catch (error) {
+    res.status(404).json({error: error.message})
+  }
+
+
+
+// try {
   
-    try {
-      
-      console.log (API_KEY);
-        var apiresult = await axios.get(`https://api.rawg.io/api/games?key=${API_KEY}`);
-        apiresult = apiresult.data.results;
-        res.status(200).json(apiresult)
-    } catch (error) {
-      res.json({error: error.message})
-    }
- 
-    
-  
+//   console.log (API_KEY);
+//     var apiresult = await axios.get(`https://api.rawg.io/api/games?key=${API_KEY}`);
+//     apiresult = apiresult.data.results;
+//     res.status(200).json(apiresult)
+// } catch (error) {
+//   res.json({error: error.message})
+// }
 }
 
 const getGameDetailHandler = async (req, res) => {
